49ee3b91b12a26a1ec6475358b77600d934308c9
Rohan Raj <1234rohanrajrr@gmail.com>
2026-08-13 13:12:29 +0530

    Add CP-31_800_6.cpp

---
+++ CP-31_800_6.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long ll;
+typedef vector<int> vi;
+#define hi cout<<"hi" 
+#define ho cout<<"ho" 
+#define he cout<<"he" 
+#define nl cout<<endl
+#define pb push_back
+#define ite(i,a,b) for (int i = a; i < b; i++)
+
+int main(){
+    fastio()
+    
+
+
+    return 0;
+}
